0a4302ba24b2d8946d5b47fde5aac1c981d2daee
LuDiemert <lucianadiemert@gmail.com>
2024-03-06 17:59:24 -0300

    feat: add list product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ export default function Home() {
   return (
 
 
-    <main>
-      <h1>
-        Test next
-      </h1>
+    <main >
+      <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+        <div className="border grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+    
+
+
+        </div>
+      </div>
     </main>
   );
 }
