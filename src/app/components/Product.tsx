c2c27b94df3cb271102598c7f5ca760dfa5a0c0b
LuDiemert <lucianadiemert@gmail.com>
2024-03-07 16:51:53 -0300

    feat: configure img the project

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,24 +1,29 @@
 import { ProductType } from "@/types/ProductType"
+import { ProductImage } from "./Productimage";
 
 
 type ProductProps = {
   product: ProductType;
 };
 
-export function Product({ product } : ProductProps)  {
+export function Product({ product }: ProductProps) {
 
   return (
-    <div className="flex flex-col shadow-lg h-96  bg-slate-700 p-5 text-gray-300"> 
+    <div className="flex flex-col shadow-lg h-96  bg-slate-700 p-5 text-gray-300">
+      <div className="relative max-h-72 flex-1">
+        <ProductImage product={product} fill />
 
-      <div className="relative max-h-72 flex-1">IMG</div>
-      <div className="flex justify-between font-bold my-3">{product.title}</div>
-      <div className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">Add Car</div>
-      
-      
-      
       </div>
-  
-  )
+      <div className="flex justify-between font-bold my-3">
+        
+       <p> {product.title} </p>
+        
+        </div>
+      <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
+        Add Car</button>
+    </div>
+
+  );
 
 
 }
